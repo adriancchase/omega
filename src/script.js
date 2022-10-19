f60322e18b5de8309021d789996dbb43dc27d7ff
anti4m <nathaniel.hansche@gmail.com>
2022-10-19 16:18:43 -0400

    Created basic home page.

---
+++ src/script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+   createPost();
+   createPost();
+   createPost();
+   createPost();
+   createPost();
+   createPost();
+}
+
+
+function createPost(attendees, start, end, location) {
+    const newPost = document.getElementById('post').content.cloneNode(true);
+    document.getElementById('feed').appendChild(newPost);
+}
