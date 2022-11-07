0928cc7ca34cc8f6e22812f1750db6dddbfa5c60
anti4m <nathaniel.hansche@gmail.com>
2022-11-07 15:42:36 -0500

    Implemented user/{userName}/friends endpoint, which is set to return sample data.

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {join} from 'path';
+import {USERS, POSTS} from '../sampleData/sampleData';
 
 
 const app = express();
@@ -10,7 +11,16 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, '../index.html'));
 });
 
+app.get('/user/:userName/friends', (req, res) => {
+  const userName = req.params.userName;
+  if (userName in USERS) {
+    res.send(USERS[userName]);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 app.listen(port, function () {
   console.log(`Server listening on port ${port}!`);
+  console.log(JSON.stringify(USERS));
 });
