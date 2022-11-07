f4263afac7ea11f9739cf11556c794e2bd66a366
malam1210 <70979253+malam1210@users.noreply.github.com>
2022-11-06 18:32:24 -0600

    Made sample users and posts and updated obj fields

--- src/sampleData/sampleData.js
+++ src/sampleData/sampleData.js
@@ -4,7 +4,37 @@ import { faker } from "@faker-js/faker";
 export const USERS = {};
 export const POSTS = {};
 export const friend = [];
-export const feed = [];
+export const attendee = [];
+export const visible = [];
+
+for (let i = 0; i < 10; i++) {
+  let fr = createRandomUser();
+  let userName = fr.username;
+  USERS[userName] = fr;
+}
+
+for (let i = 0; i < 10; i++) {
+  let po = createRandomPost();
+  let poID = po.id;
+  POSTS[poID] = po;
+}
+
+Array.from({ length: 10 }).forEach(() => {
+  friend.push(faker.internet.userName());
+});
+Array.from({ length: 10 }).forEach(() => {
+  attendee.push(faker.internet.userName());
+});
+Array.from({ length: 10 }).forEach(() => {
+  visible.push(faker.internet.userName());
+});
+export const feedArr = [];
+
+const postsSize = Object.keys(POSTS).length;
+
+for (const post in POSTS) {
+  feedArr.push(post.id);
+}
 
 export function createRandomUser() {
   const startDate = faker.date.soon(10, "2020-01-01T00:00:00.000Z");
@@ -19,13 +49,9 @@ export function createRandomUser() {
     lastName: faker.name.lastName,
     posts: [],
     availability: { start: startDate, end: endDate },
-    friends: Array.from({ length: 10 }).forEach(() => {
-      friend.push(faker.internet.userName());
-    }),
+    friends: friend,
     pictureUrl: faker.image.avatar(),
-    feed: Array.from({ length: 10 }).forEach(() => {
-      friend.push(faker.internet.userName());
-    }),
+    feed: feedArr,
   };
 }
 
@@ -38,20 +64,10 @@ export function createRandomPost() {
   return {
     id: faker.datatype.uuid(),
     author: faker.internet.userName(),
-    attendees: [],
+    attendees: attendee,
     location: "Amherst",
     timeInterval: { start: startDate, end: endDate },
+    chatId: faker.datatype.uuid(),
+    visibleTo: visible,
   };
-}
-
-for (let i = 0; i < 10; i++) {
-  let fr = createRandomUser();
-  let userName = fr.username;
-  USERS[userName] = fr;
-}
-
-for (let i = 0; i < 10; i++) {
-  let po = createRandomPost();
-  let poID = po.id;
-  POSTS[poID] = po;
 }
