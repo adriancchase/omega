bcd2c70c48a351d57265500bb9cfbf398730be82
malam1210 <70979253+malam1210@users.noreply.github.com>
2022-11-09 04:26:22 -0600

    Login local storage

---
+++ src/login.js
@@ -0,0 +1,37 @@
+function Login() {
+  const a = new Array();
+  const up1 = new Object();
+  const up2 = new Object();
+
+  up1 = {
+    name: "[abcd@gmail.com](mailto:abcd@gmail.com)",
+    password: btoa("abc@12"),
+  };
+
+  up2 = {
+    name: "[bcd@gmail.com](mailto:bcd@gmail.com)",
+    password: btoa("bcd@12"),
+  };
+
+  a.push(up1);
+  a.push(up2);
+
+  const username = document.getElementById("exampleInputEmail1").value;
+  const password = document.getElementById("exampleInputPassword1").value;
+
+  sessionStorage.setItem("currentloggedin", username);
+
+  localStorage.setItem("all_users", JSON.stringify(a));
+
+  a = JSON.parse(localStorage.getItem("all_users"));
+
+  a.push({ name: username, password: password });
+
+  localStorage.setItem("name", JSON.stringify(a));
+
+  for (var i = 0; i < a.length; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = a[i]["name"];
+    document.getElementById("listuser").appendChild(li);
+  }
+}
