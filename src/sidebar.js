c293bd694cf89da3e875b02610878ff72c5e8113
anti4m <nathaniel.hansche@gmail.com>
2022-11-09 22:01:14 -0500

    Fixed sidebar to set username after the rest of the element is loaded.

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,5 +2,6 @@ import {importHtml} from './utils/htmlUtils.js';
 import {getLoggedInUserName} from './utils/dataUtils.js';
 
 
-importHtml('sidebar.html', 'createSidebar', ['col-2', 'p-3', 'bg-light']);
-document.getElementById('loggedInUserName').innerText = getLoggedInUserName();
+importHtml('sidebar.html', 'createSidebar', ['col-2', 'p-3', 'bg-light']).then(() => {
+    document.getElementById('loggedInUserName').innerText = getLoggedInUserName();
+});
