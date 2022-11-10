9f300d32e07f0eda7e040dcb5993eefbeda72293
anti4m <nathaniel.hansche@gmail.com>
2022-11-09 21:58:19 -0500

    Modified sidebar to show logged in user's username.

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,4 +1,6 @@
-import { importHtml } from './utils/htmlUtils.js';
+import {importHtml} from './utils/htmlUtils.js';
+import {getLoggedInUserName} from './utils/dataUtils.js';
 
 
 importHtml('sidebar.html', 'createSidebar', ['col-2', 'p-3', 'bg-light']);
+document.getElementById('loggedInUserName').innerText = getLoggedInUserName();
