e6c86ddd2e97a8340c8d21d579fc2248bcac61e6
anti4m <nathaniel.hansche@gmail.com>
2022-11-20 20:28:31 -0500

    Modified POST post/new and GET post/{id} endpoints to connect to database.

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -4,6 +4,7 @@ import { URL, fileURLToPath } from "url";
 import { USERS, POSTS } from "../sampleData/sampleData.js";
 import * as sampleData from "../sampleData/sampleData.js";
 import { userRouter } from "./routes/user.router.js";
+import { postRouter } from "./routes/post.router.js";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const app = express();
@@ -11,24 +12,11 @@ const port = process.env.PORT ?? 8080;
 
 app.use(express.static(join(__dirname, "../../src")));
 app.use("/user", userRouter);
+app.use("/post", postRouter);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "../../src/index.html"));
-});
-
-
-app.post("/post/new", (req, res) => {
-  console.log(req);
-  const post = req.body;
-  post.id = Object.keys(POSTS).length;
-  POSTS[post.id] = post;
-
-  const authorUser = USERS[post.author];
-  authorUser.feed.unshift(post.id);
-  authorUser.posts.unshift(post.id);
-
-  res.sendStatus(200);
 });
 
 //Returns all posts for listing in calendar
@@ -37,14 +25,6 @@ app.get("post/:userName", (req, res) => {
   res.send(postsByUser);
 });
 
-app.get("/post/:id", (req, res) => {
-  const postId = req.params.id;
-  if (postId in POSTS) {
-    res.send(POSTS[postId]);
-  } else {
-    res.sendStatus(404);
-  }
-});
 
 app.listen(port, function () {
   console.log(`Server listening on port ${port}!`);
