f12597f2011495a1f1a640e9284f4448ef9c6d29
anti4m <nathaniel.hansche@gmail.com>
2022-11-30 12:46:48 -0500

    Removed files from old directory structure.

--- src/backend/models/TimeInterval.ts
+++
@@ -1,8 +0,0 @@
-export interface TimeInterval {
-    start: Date;
-    end: Date;
-}
-
-export function isTimeInterval(obj: Object) {
-    return 'start' in obj && 'end' in obj;
-}
--- src/utils/dataUtils.js
+++
@@ -1,3 +0,0 @@
-export function getLoggedInUserName() {
-    return 'nhansche';
-}
