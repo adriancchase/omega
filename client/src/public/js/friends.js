ef6e29e8fafe14f18dcd685c6edf78d4c4ebcb62
anti4m <nathaniel.hansche@gmail.com>
2022-12-10 17:19:29 -0500

    Connected friends list to backend, improved styling, and added a default profile picture.

--- client/src/public/js/friends.js
+++ client/src/public/js/friends.js
@@ -1,12 +1,38 @@
-window.onload = () => {
-  for (let i = 0; i < 25; ++i) {
-    createFriendItem();
-  }
+import { getLoggedInUserName } from './utils/dataUtils.js';
+
+
+window.onload = async () => {
+  await displayFriends();
 };
 
-function createFriendItem(attendees, start, end, location) {
-  const newPost = document
-    .getElementById("friend-item")
-    .content.cloneNode(true);
-  document.getElementById("friends-list").appendChild(newPost);
+
+async function displayFriends() {
+  document.getElementById('friends-list').innerHTML = '';
+  const userName = getLoggedInUserName();
+  const friends = await fetch(`/user/${userName}/friends`).then(res => res.json());
+  console.log(JSON.stringify(friends));
+  if (friends.length) {
+      friends.forEach(createFriendItem);
+  } else {
+      // Display a message if user has received no invitations.
+      const messageDiv = document.createElement('div');
+      messageDiv.style.textAlign = 'center';
+      messageDiv.innerText = 'You have no friends!';
+      document.getElementById('friends-list').appendChild(messageDiv);
+  }
+}
+
+function createFriendItem(friendUserView) {
+  const newFriend = document.getElementById('friend-item').content.cloneNode(true);
+  const friendPictureUrl = newFriend.querySelector('#friendPic');
+  if (friendUserView.pictureUrl.length) {
+    friendPictureUrl.src = friendUserView.pictureUrl;
+  } else {
+    friendPictureUrl.src = '/images/default_profile_pic.png';
+  }
+
+  const friendName = newFriend.querySelector('#friendName');
+  friendName.innerText = `${friendUserView.firstName} ${friendUserView.lastName}`;
+
+  document.getElementById('friends-list').appendChild(newFriend);
 }
