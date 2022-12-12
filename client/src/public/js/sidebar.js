76d3eb249af6443dac6ab3ba3ffe25b031764bf3
anti4m <nathaniel.hansche@gmail.com>
2022-12-11 19:12:07 -0500

    Set sidebar to display profile picture from database.

--- client/src/public/js/sidebar.js
+++ client/src/public/js/sidebar.js
@@ -1,7 +1,13 @@
 import {importHtml} from './utils/htmlUtils.js';
-import {getLoggedInUserName} from './utils/dataUtils.js';
+import {getLoggedInUserName, jsonFetch} from './utils/dataUtils.js';
 
 
-importHtml('sidebar.html', 'createSidebar', ['col-2', 'p-3', 'bg-light']).then(() => {
+importHtml('sidebar.html', 'createSidebar', ['col-2', 'p-3', 'bg-light']).then(async () => {
+    const userName = getLoggedInUserName();
     document.getElementById('loggedInUserName').innerText = getLoggedInUserName();
+
+    const user = await jsonFetch(`/user/${userName}`, 'GET').then(res => res.json());
+    if (user && user.pictureUrl && user.pictureUrl.length) {
+        document.getElementById('profilePic').src = user.pictureUrl;
+    }
 });
