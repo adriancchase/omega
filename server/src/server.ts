b701b5bd0c839b379fd9fa6088a360b13dc3bb75
anti4m <nathaniel.hansche@gmail.com>
2022-12-11 21:44:00 -0500

    Removed sampleData.ts import in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import { join } from 'path';
 import { URL, fileURLToPath } from 'url';
-import { USERS, POSTS } from './sampleData.js';
-import * as sampleData from './sampleData.js';
 import { userRouter } from './routes/user.router.js';
 import { postRouter } from './routes/post.router.js';
 
@@ -19,15 +17,7 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, '../../client/src/public/html/index.html'));
 });
 
-//Returns all posts for listing in calendar
-app.get('post/:userName', (req, res) => {
-  const postsByUser = sampleData.getPostsByUser(req.params.userName);
-  res.send(postsByUser);
-});
-
 
 app.listen(port, function () {
   console.log(`Server listening on port ${port}!`);
-  console.log(JSON.stringify(USERS, null, 2));
-  console.log(JSON.stringify(POSTS, null, 2));
 });
